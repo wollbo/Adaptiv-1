3e842c3018a1a33ef66d2e309442e82a372a4a4a
Hilding Wollbo <Hilding@n149-p171.eduroam.kth.se>
2019-02-02 13:52:23 +0100

    test1

--- test1.m
+++ test1.m
@@ -1,11 +1,16 @@
-%% spectrum of whole sequence %%
-
-
+%% picks out e(n) %%
 
 [z, fs] = audioread('EQ2401project1data2019.wav');
-
+z_act = z(42000:51000);
+% soundsc(z_act)
 %%
 
-vs = [0:1/length(z):1];
+vs = [0:1/length(z_act):1];
 
-plot(vs(1:end-1), abs(fft(data)))
+z_spec = abs(fft(z_act))
+
+plot(z)
+plot(vs(1:round(end/2)), z_spec(1:round(end/2)))
+
+
+%% most noise seems to be confined at frequencies lower than 120 Hz
