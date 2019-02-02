abc11f7571f8788acde7f86c52ee90300df20676
Hilding Wollbo <Hilding@n149-p171.eduroam.kth.se>
2019-02-02 17:46:02 +0100

    fix sep

--- separate.m
+++ separate.m
@@ -1,9 +1,9 @@
 function [y_sep, e_sep] = separate(y)
 
-noise_indexes = [1:4500 19000:26000 42000:51000] 
-signal_indexes = [4501:18999 26001:41999 51001:length(y)]
+noise_indexes = [1:4500 19000:26000 42000:51000];
+signal_indexes = [4501:18999 26001:41999 51001:length(y)];
 
-e_sep = y(noise_indexes)
-y_sep = y(signal_indexes)
+e_sep = y(noise_indexes);
+y_sep = y(signal_indexes);
 
 end
