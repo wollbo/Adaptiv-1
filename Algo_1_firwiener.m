ea9d92871121ca4e6905ab2a3be6035a4e11a8b3
PVJesper <jesper_brunnstrom@hotmail.com>
2019-02-09 17:09:00 +0100

    ncfilt

--- Algo_1_firwiener.m
+++ Algo_1_firwiener.m
@@ -1,7 +1,8 @@
 %Load Data
 [z, Fs] = audioread('EQ2401project1data2019.wav');
 %e = [y(1:4500); y(19000:26000); y(42000:51000)];
-[y,e] = separate(z);
+e = z(42000:51000);
+y = [4501:18999 26001:41999 51001:length(y)];
 
 %Apply Filter
 [ryy, rxy] = estimateakf(y, e, 3000);
