8617f8ee254d04c7ee6ab4410c629deac31cff78
PVJesper <jesper_brunnstrom@hotmail.com>
2019-02-09 13:56:06 +0100

    ncwiener in freq domain again

--- AnalyzeSignals.m
+++ AnalyzeSignals.m
@@ -1,6 +1,6 @@
 %Load Data
-[y, Fs] = audioread('EQ2401project1data2019.wav');
-[y,e] = separate(y);
+[z, Fs] = audioread('EQ2401project1data2019.wav');
+[y,e] = separate(z);
 
 figure()
 hold on
@@ -26,21 +26,34 @@ fprintf('var{e(n)} = %f \nvar{y(n)} = %f \n', vare, vary);
 %Frequency response of y over time
 figure('Position', [100,100, 1500, 500])
 framelen = floor(20e-3*Fs);
-ulen = 32;
-numFrames = floor((length(y)-framelen) / ulen) + 1;
+ulen = 64;
+numAvg = 6;
+avgCounter = 1;
 fftlen = 1024;
+prevFrames = zeros(fftlen, numAvg-1);
+numFrames = floor((length(y)-framelen) / ulen) + 1;
+
 for i = 1:numFrames
     subplot(1,2,2)
+
     n = (i-1)*ulen+1;
-    frame = y(n:n+framelen);
-    freqresp = abs(fft(frame, fftlen)).^2;
     
-    plot(linspace(0,0.5*Fs, fftlen/2), 10*log10(freqresp(1:fftlen/2)));
+    frame = y(n:n+framelen-1).*hanning(framelen);
+    freqresp = abs(fft(frame, fftlen)).^2;
+    avgFreqresp = mean([freqresp, prevFrames], 2);
+    
+    prevFrames(:,avgCounter) = freqresp;
+    avgCounter = mod(avgCounter,numAvg) + 1;
+    
+    plot(linspace(0,0.5*Fs, fftlen/2), 10*log10(avgFreqresp(1:fftlen/2)));
+    grid on
+    box off
     axis([0, 0.5*Fs, -30, 30]);
    
     
     subplot(1,2,1)
     plot(y);
-    rectangle('Position', [n, -30, framelen, 60], 'FaceColor', [0.5 0.5 0.5 0.5], 'EdgeColor', [1 1 1])
+    rectangle('Position', [n, min(y), framelen, max(y)-min(y)], 'FaceColor', [0.3 1 0.4 0.9], 'EdgeColor', [1 1 1, 0])
+    axis off
     drawnow
 end
--- ncwiener.m
+++ ncwiener.m
@@ -1,22 +1,22 @@
 function [xhat, H] = ncwiener(y, ryy, rxy)
 assert (length(ryy) == length(rxy))
-xhat = filter(rxy, ryy, y);
+% xhat = filter(rxy, ryy, y);
 
 %Get filter frequency response
-fftlen = 1024;
-phiyy = fft(ryy, fftlen);
-phixy = fft(rxy, fftlen);
-H = phixy ./ phiyy;
+% fftlen = 1024;
+% phiyy = fft(ryy, fftlen);
+% phixy = fft(rxy, fftlen);
+% H = phixy ./ phiyy;
 
 
 %Old
-% fftlen = 2^nextpow2(length(y) + length(ryy) - 1);
-% 
-% phiyy = fft(ryy, fftlen);
-% phixy = fft(rxy, fftlen);
-% 
-% H = phixy ./ phiyy;
-% Y = fft(y, fftlen);
-% Xhat = Y .* H;
-% xhat = ifft(Xhat);
+fftlen = 2^nextpow2(length(y) + length(ryy) - 1);
+
+phiyy = fft(ryy, fftlen);
+phixy = fft(rxy, fftlen);
+
+H = phixy ./ phiyy;
+Y = fft(y, fftlen);
+Xhat = Y .* H;
+xhat = ifft(Xhat);
 end
